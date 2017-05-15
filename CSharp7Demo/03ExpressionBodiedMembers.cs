0cd9fb5f14714c12478f6c60fc863803be96b1a1
Robert Haken <haken@havit.cz>
2017-05-15 10:43:16 +0200

    Expression-bodied members - upgrade

--- CSharp7Demo/03ExpressionBodiedMembers.cs
+++ CSharp7Demo/03ExpressionBodiedMembers.cs
@@ -10,13 +10,26 @@ namespace CSharp7Demo
 	{
 		private string fullname;
 
-		//public string Fullname => fullname;
+		// C# 6.0 - Expression-bodied Methods and Properties
+		public override string ToString() => fullname;
+		public string FullnameX => fullname;  // getter-only
+
+		//
+		// 
+		// C# 7.0 - More expression-bodied members
+		
+		// Expression-bodied constructor
+		public ExpressionBodiedMembers(string fullname) => this.fullname = fullname;
+
+		// Expression-bodied destructor (finalizer)
+		~ExpressionBodiedMembers() => Console.Error.WriteLine("Finalized!");
+
+		// Expression-bodied get/set accessors
 		public string Fullname
 		{
 			get => fullname;
 			set => fullname = value;
 		}
 
-		public override string ToString() => fullname;
 	}
 }
